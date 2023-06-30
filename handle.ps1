d5c61aa081bdb1fe17dbfe02e0667019aea1ddc4
devabacus <devabacusnrf@gmail.com>
2023-06-30 08:34:17 +0530

    init

---
+++ handle.ps1
@@ -0,0 +1,16 @@
+python -m venv env
+.\env\Scripts\Activate.ps1
+pip install django
+pip install gunicorn
+django-admin startproject touristhelper
+cd touristhelper
+python -m pip freeze > requirements.txt
+
+python manage.py startapp touristapp
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8350
+
+docker compose up --build
+
+pip install django-environ
+
